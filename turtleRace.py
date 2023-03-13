735b6904c222d3c73b271d27e6bd33aaa0aa26c2
yas-say <yasowant@live.com>
2023-03-12 21:46:53 -0400

    new file updated

--- turtleRace.py
+++ turtleRace.py
@@ -10,7 +10,7 @@ screen.setup(width=500, height=500)
 user_bet = screen.textinput(title="Enter your bet...", prompt="Which color turtle will win?")
 print(user_bet)
 
-tim=[]
+allTurtle=[]
 print(screen.window_height())
 xcord = -230
 ycord = ((screen.window_height()/2)-30)* -1
@@ -19,18 +19,19 @@ for _ in colors:
     myturtle = Turtle(shape="turtle")
     myturtle.color(_)
     myturtle.penup()
-    tim.append(myturtle)
+    allTurtle.append(myturtle)
     myturtle.goto(x=-230, y= ycord)
     ycord+= (screen.window_height()/len(colors))
 winner=""
 flag = True
 while flag:
-    for _ in tim:
+    for _ in allTurtle:
         _.forward(randint(0,10))
         if _.xcor() >= 250:
             flag = False
-            winner = _.color()
+            winner = _.pencolor()
             break
+
 
 print(f"Winner is {winner}")
 
