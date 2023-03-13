1606ec80e8292cb9b0ca8aeab7593452f3d6844a
yas-say <yasowant@live.com>
2023-03-12 21:01:43 -0400

    new

---
+++ etchASketch.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def w_move_forward():
+    tim.forward(10)
+
+
+def s_move_backward():
+
+    tim.backward(10)
+
+
+def a_counter_clockwise():
+    tim.left(5)
+    tim.forward(10)
+
+
+def d_clockwise():
+    tim.right(5)
+    tim.forward(10)
+
+def c_clear():
+    tim.reset()
+
+screen.listen()
+
+screen.onkey(key="w", fun=w_move_forward)
+screen.onkey(key="s", fun=s_move_backward)
+screen.onkey(key="a", fun=a_counter_clockwise)
+screen.onkey(key="d", fun=d_clockwise)
+screen.onkey(key="c", fun=c_clear)
+
+screen.exitonclick()
